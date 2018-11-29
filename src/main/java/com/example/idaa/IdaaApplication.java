a3859ffd221ef068a268036be93ed2d57aa2c30b
zzq <314005311@qq.com>
2018-11-29 10:25:10 +0800

    Initial commit

---
+++ src/main/java/com/example/idaa/IdaaApplication.java
@@ -0,0 +1,12 @@
+package com.example.idaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdaaApplication.class, args);
+    }
+}
